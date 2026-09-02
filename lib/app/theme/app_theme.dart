b5901a782169004133a452df3a580e9947565abb
Braglath Sreekumar <87694668+braglath@users.noreply.github.com>
2026-09-02 19:55:26 +0530

    app texts in constants

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/app/theme/app_colors.dart';
 import 'package:portfolio/app/theme/app_text_styles.dart';
+import 'package:portfolio/core/constants/app_texts.dart';
 
 class AppTheme {
   AppTheme._();
@@ -8,7 +9,7 @@ class AppTheme {
   static ThemeData light = ThemeData(
     useMaterial3: true,
 
-    fontFamily: 'Geist',
+    fontFamily: AppTexts.geist,
 
     scaffoldBackgroundColor: AppColors.background,
 
---
+++ lib/core/constants/app_texts.dart
@@ -0,0 +1,4 @@
+class AppTexts {
+  AppTexts._();
+  static const String geist = 'Geist';
+}
