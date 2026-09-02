6890caa24607567d234d45083f2c7318b4bd4e1e
Braglath Sreekumar <87694668+braglath@users.noreply.github.com>
2026-09-02 16:20:49 +0530

    added logger and routes and packages

---
+++ lib/app/navigation/app_routes.dart
@@ -0,0 +1,35 @@
+// app_router.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:portfolio/app/app.dart';
+import 'package:portfolio/app/navigation/route_tracker.dart';
+import 'package:portfolio/core/utils/logger_utils.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    debugLogDiagnostics: true,
+    initialLocation: AppRoutes.splash,
+    observers: [routeTracker],
+    redirect: (context, state) {
+      final loc = state.uri.toString();
+      log.info("router loc: $loc");
+
+      // return AppRoutes.home; // hardcoded navigation for testing
+
+      return null;
+    },
+    routes: [
+      GoRoute(
+        name: 'home',
+        path: AppRoutes.home,
+        builder: (_, _) => const PortfolioApp(),
+      ),
+    ],
+  );
+});
+
+class AppRoutes {
+  AppRoutes._();
+  static const splash = '/splash';
+  static const home = '/home';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/app/app.dart';
+import 'package:portfolio/core/utils/logger_utils.dart';
 
 void main() {
-  runApp(const PortfolioApp());
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (kDebugMode) {
+      FlutterError.presentError(details);
+    } else {
+      log.error(
+        'Flutter framework error',
+        error: details.exception,
+        stackTrace: details.stack,
+      );
+    }
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    log.error('PlatformDispatcher error', error: error, stackTrace: stack);
+    return true;
+  };
+
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized(); // ← inside zone
+      runApp(const ProviderScope(child: PortfolioApp()));
+    },
+    (error, stack) {
+      log.error('Uncaught zone error', error: error, stackTrace: stack);
+    },
+  );
 }
